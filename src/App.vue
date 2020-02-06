18e4a1d56d7db202a7cba2a140a1f732ac9c81a7
lisongda96 <2446479874@qq.com>
2020-02-06 12:43:01 +0800

    storage

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,16 @@
 </template>
 
 <script>
-
+// import storage from './storage/index'
 export default {
   name: 'app',
   components: {
+  },
+  mounted(){
+    // storage.setItem('a',1)
+    // storage.setItem('user', {a:1})
+    // storage.setItem('abc', {a:1}, 'user');
+    // storage.clear('a', 'user')
   }
 }
 </script>
